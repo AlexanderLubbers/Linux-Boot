6aa62609be4b0923fc3e3eb470e517856a263424
Alexander Lubbers <alexanderb.lubbers@gmail.com>
2026-08-27 14:47:02 -0400

    rsdp location logic draft and makefile bug fix

--- Makefile
+++ Makefile
@@ -12,11 +12,11 @@ all:
 	gcc -m64 -ffreestanding -c ./src/fat_driver.c -o ./elf/fat_driver.o
 	gcc -m64 -ffreestanding -c ./src/rsdp.c -o ./elf/rsdp.o
 	ld -T linker.ld ./elf/loader.o ./elf/fat_driver.o ./elf/rsdp.o -o ./elf/loader.elf
-	@echo "elf loader is $(ELF_SIZE) bytes"
-	@echo "number of sectors in elf loader $(ELF_SECTORS)"
+	@echo "elf loader is $(ELF_LOADER_SIZE) bytes"
+	@echo "number of sectors in elf loader $(ELF_LOADER_SECTORS)"
 	nasm -f bin ./src/elf_loader.asm -o ./bin/elf_loader.bin
-	@echo "stage 2 is $(ELF_LOADER_SIZE) bytes"
-	@echo "number of sectors in stage 2 $(ELF_LOADER_SECTORS)"
+	@echo "stage 2 is $(ELF_SIZE) bytes"
+	@echo "number of sectors in stage 2 $(ELF_SECTORS)"
 	nasm -f bin -d SECTORS=$(ELF_LOADER_SECTORS) ./src/bootloader.asm -o ./bin/boot.bin
 	cat ./bin/boot.bin ./bin/elf_loader.bin ./elf/loader.elf > ./elf/bootloader.img
 clean:
@@ -32,11 +32,11 @@ debug:
 	gcc -m64 -ffreestanding -c ./src/fat_driver.c -o ./elf/fat_driver.o
 	gcc -m64 -ffreestanding -c ./src/rsdp.c -o ./elf/rsdp.o
 	ld -T linker.ld ./elf/loader.o ./elf/fat_driver.o ./elf/rsdp.o -o ./elf/loader.elf
-	@echo "stage 2 is $(ELF_SIZE) bytes"
-	@echo "number of sectors $(ELF_SECTORS)"
+	@echo "elf loader is $(ELF_LOADER_SIZE) bytes"
+	@echo "number of sectors in elf loader $(ELF_LOADER_SECTORS)"
 	nasm -f bin ./src/elf_loader.asm -o ./bin/elf_loader.bin
-	@echo "stage 2 is $(ELF_LOADER_SIZE) bytes"
-	@echo "number of sectors $(ELF_LOADER_SECTORS)"
+	@echo "stage 2 is $(ELF_SIZE) bytes"
+	@echo "number of sectors in stage 2 $(ELF_SECTORS)"
 	nasm -f bin -d SECTORS=$(ELF_LOADER_SECTORS) ./src/bootloader.asm -o ./bin/boot.bin
 	nasm -f elf32  -DDEBUG -g -F dwarf ./src/elf_loader.asm -o ./elf/elfdebug.o
 	ld -m elf_i386 -Ttext 0x0500 -o ./elf/elfdebug.elf ./elf/elfdebug.o
--- src/rsdp.c
+++ src/rsdp.c
@@ -1,6 +1,80 @@
+#include <stdint.h>
 
-
-int placeholder() {
-
+// search for the rsdp signature in memory region 0x000E0000 to 0x000FFFFF
+// return location of rsdp table if found, else return 0
+uintptr_t search_memory() {
+    uintptr_t address = 0x000e0000;
+    // note it is possible for RSDP to be at this address
+    uintptr_t end_address = 0x000fffff;
+    char rsdp_signature[8] = {'R', 'S', 'D', ' ', 'P', 'T', 'R', ' '};
+    while (address <= end_address) {
+        volatile char *ptr = (volatile char *)address;
+        int same = 1;
+        for (int i = 0; i < 8; i++) {
+            char val = *ptr;
+            if (val != rsdp_signature[i]) {
+                same = 0;
+                break;
+            }
+            ptr = ptr + 1;
+        }
+        if (same == 1) return address;
+        address = address + 16; // rsdp signature is found on 16 byte boundary
+    }
     return 0;
+}
+// search the extended BIOS Data Area for rsdp signature
+// return location of rsdp table if found, else return 0
+uintptr_t search_EBDA() {
+    uintptr_t ebda_location = 0x40e;
+    volatile uint16_t ebda_address = *(volatile uint16_t *)ebda_location;
+    uintptr_t ebda = ebda_address;
+    char rsdp_signature[8] = {'R', 'S', 'D', ' ', 'P', 'T', 'R', ' '};
+    while (ebda <= ebda + 0x400) {
+        volatile char *ptr = (volatile char *)ebda;
+        int same = 1;
+        for (int i = 0; i < 8; i++) {
+            char val = *ptr;
+            if (val != rsdp_signature[i]) {
+                same = 0;
+                break;
+            }
+            ptr = ptr + 1;
+        }
+        if (same == 1) return ebda;
+        ebda = ebda + 16;
+    }
+    return 0;
+}
+
+void find_rsdp() {
+    uintptr_t result = search_memory();
+    if (result != 0) {
+        __asm__ (
+            "xor %%rax, %%rax\n\t"
+            "mov %%rax, %0"
+            :
+            : "r" (result)
+            : "rax"
+        );
+        return;
+    }    
+    result = search_EBDA();
+    if (result != 0) {
+        __asm__ (
+            "xor %%rax, %%rax\n\t"
+            "mov %%rax, %0"
+            :
+            : "r" (result)
+            : "rax"
+        );
+        return;
+    }
+    __asm__(
+        "xor %%rax, %%rax"
+        :
+        :
+        : "rax"
+    );
+    return;
 }
